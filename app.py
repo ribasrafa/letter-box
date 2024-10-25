0e528dff8fa6bb8b6e45c3c387545dd594c549eb
seu_usuario <seu_nome@email.com>
2024-10-25 15:30:20 -0300

    login funcional

--- app.py
+++ app.py
@@ -2,11 +2,23 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+# criar uma lista de usuarios e senha, depois vamos pegar no DB
+usuarios = {
+    'admin' : 'admin',
+    'usuario' : 'senha',
+    'rafaela' : '111111',
+}
+
 # Definindo a rota principal do site
 @app.route('/')
-def home ()
+def home ():
     return render_template('index.html')
 
+# Definindo a rota principal do site
+@app.route('/login')
+def login ():
+    return render_template('login.html')
+
 # Parte principal do programa em Python
-if __name__ =='__main__':
-    app.run(debug-True)
+if __name__ == '__main__':
+    app.run(debug=True)
