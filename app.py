4237d0af494983ab36ce6388c5a6b78e3986fa0a
seu_usuario <seu_nome@email.com>
2024-10-29 16:41:18 -0300

    verificação do login

--- app.py
+++ app.py
@@ -19,6 +19,20 @@ def home ():
 def login ():
     return render_template('login.html')
 
+#Verificar o login
+@app.route('/verificar-login', methos=['POST'])
+def verificar_login():
+
+#Pegando o que o usuario digitou no campo de entrada de user e senha
+    username = request.form['username']
+    password = request.form['password']
+
+#Verificar se o usuario digitado está na lista e se a senha está certa
+if username in usuarios and usuarios[username] == password:
+    return f"Bem-vindo, {username}!"
+else:
+    return "Usuário ou senha inválidos."
+
 # Parte principal do programa em Python
 if __name__ == '__main__':
     app.run(debug=True)
